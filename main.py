cb3b23d80c6caf8693c3b2fe2b0b5c58638f6d35
Gabriela <gabedeandradee@gmail.com>
2022-08-24 17:01:48 -0300

    criação da main e alguns detalhes

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-print("Teste")
-a = 9
-b = 7
+import random
+def main(palavra):
+    compara = embaralha(palavra)
+
+    print("Você tem 5 tentativas para adivinhar qual é a palavra a seguir: "+compara)
+    tentativa = ""
+    for i in range(5):
+        if tentativa != palavra:
+            if i < 4:
+                tentativa = input("Tentativa "+ f"{i+1}" +": ")
+                print("Você ainda tem "+ f"{4-i}" +" tentativas! " + motivacional())
+            else:
+                tentativa = input("Tentativa "+ f"{i+1}" +": ")
+                print("Você ainda tem "+ f"{4-i}" +" tentativa! " + motivacional())
+        else:
+            print("Parabéns! Você acertou.")
+
+    print("Você falhou! A palavra era: " + palavra)
+
+def embaralha(p):
+    mistura = []
+    for letra in p:
+        mistura.append(letra)
+    mistura.sort()
+    final = "".join(mistura)
+    return final
+
+def motivacional():
+    frases = ["Você consegue!",
+            "Não desista!",
+            "Acredito em você!",
+            "Falta pouco!"]
+    return frases[random.randint(0,3)]
+
+
+main("papagaio")
