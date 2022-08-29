540512a5b8a5624f1ad7c445ff14cc56dfd7dfd3
Gabriela <gabedeandradee@gmail.com>
2022-08-29 16:14:52 -0300

    implementando dificuldade

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def main():
     dificuldade = input("Digite o número correspondente a dificuldade desejada: \n 1- Fácil \n 2- Médio \n 3- Difícil \n")
     tema = input("\n Digite o número correspondente ao tema desejado: \n 1- Alimentos \n 2- Animais \n 3- Cidade \n 4- Marcas \n 5- Objetos \n 6- Países \n")
 
-    palavra = (temas(tema)).lower()
+    palavra = (temas(dificuldade,tema)).lower()
+    
 
     global Gameover
     print("\nVocê tem 5 tentativas para adivinhar qual é a palavra a seguir: ")
@@ -59,7 +60,7 @@ def motivacional():
     motiv = frases[random.randint(0, 3)]
     print("\nVocê ainda tem " + f"{vidas}" + " tentativa(s)! " + motiv)
 
-def temas(n):
+def temas(nivel,tema):
     alimentos = ["Alface","Bolacha","Chocolate","Dobradinha","Espaguete","Farofa","Gelatina","Hamburguer","Iogurte","Jaca","Kiwi","Leite","Mandioca","Nachos","Ovo","Patê","Queijo","Rúcula","Sorvete","Tangerina","Uva","Vatapá","Yakisoba"]
     animais = ["Anta","Andorinha","Bode","Camelo","Cachorro","Dromedário","Dinossauro","Escorpião","Foca","Formiga","Gato","Girafa","Hipopótamo","Iguana","Jabuti","Leão","Lagartixa","Morcego","Ovelha","Pato","Pernilongo","Quati","Tamanduá","Urso","Vespa"]
     cidades = ["Adamantina","Aracaju","Antas","Bagé","Bandeirantes","Barueri","Chapecó","Canavieiras","Curitiba","Diamantina","Exu","Florianópolis","Franca","Goiânia","Itajaí","Itu","Natal","Palmas","Recife","Sorocaba","Teresina","Umuarama","Varginha"]
@@ -67,20 +68,39 @@ def temas(n):
     objetos = ["Agulha","Alfinete","Bola","Borracha","Copo","Caneta","Dado","Dedal","Escova","Esmalte","Espelho","Faca","Fornalha","Gaiola","Garfo","Holoforte","Imã","Isqueiro","Jarra","Luva","Lapiseira","Maquiagem","Navalha","Óculos","Pedra","Quadro","Régua","Urna","Ventilador","Vassoura","Xícara","Zíper"]
     paises = ["Alemanha","Angola","Argentina","Brasil","Bélgica","Canadá","Catar","Chile","China","Colômbia","Dinamarca","Egito","Eslováquia","Guatemala","Haiti","Iraque","Madagascar","Moçambique","Panamá","Paquistão","Peru","Portugal","Rússia","Tailândia","Ucrânia","Vaticano","Zâmbia"]
 
-    if n == "1":
-        return random.choice(alimentos)
-    elif n == "2":
-        return random.choice(animais)
-    elif n == "3":
-        return random.choice(cidades)
-    elif n == "4":
-        return random.choice(marcas)
-    elif n == "5":
-        return random.choice(objetos)
-    elif n == "6":
-        return random.choice(paises)
+    if tema == "1":
+        return random.choice(filtra_dificuldade(nivel,alimentos))
+    elif tema == "2":
+        return random.choice(filtra_dificuldade(nivel,animais))
+    elif tema == "3":
+        return random.choice(filtra_dificuldade(nivel,cidades))
+    elif tema == "4":
+        return random.choice(filtra_dificuldade(nivel,marcas))
+    elif tema == "5":
+        return random.choice(filtra_dificuldade(nivel,objetos))
+    elif tema == "6":
+        return random.choice(filtra_dificuldade(nivel,paises))
     else:
         return "\nNúmero inválido! Por favor, insira um número de 1 a 6\n"
+
+def filtra_dificuldade(nivel, lista):
+
+    if nivel == "1":
+
+        filtro = [elemento for elemento in lista if len(elemento) <= 4]  
+        return filtro
+
+    if nivel == "2":
+
+        filtro = [elemento for elemento in lista if len(elemento) > 5 and len(elemento) < 7]  
+        return filtro
+        
+    if nivel == "3":
+
+        filtro = [elemento for elemento in lista if len(elemento) >= 7]  
+        return filtro
+
+ 
 
 main()
 
